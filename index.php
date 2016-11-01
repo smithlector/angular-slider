bc5f77e86ad703bb7d66e85b4cd8ba1a5cf817e1
Prembeagle <prem.rai@burpy.com>
2016-11-01 19:31:59 +0530

    angular slider

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html ng-app="myApp">
+	<head>
+		<title>Slider - AngularJs</title>
+		<link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"/>
+		<link rel="stylesheet" href="css/style.css"/>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
+	</head>
+	<body>
+		<slider class="ng-slider-wrapper"></slider>
+		<script type="text/javascript" src="script/script.js"></script>
+	</body>
+</html>
