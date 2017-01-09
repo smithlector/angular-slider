c0d701ae3dae26304c595119c36e024ba765fa87
Prembeagle <prem.rai@burpy.com>
2017-01-09 11:39:02 +0530

    started

--- script/slider.js
+++ script/slider.js
@@ -1,6 +1,6 @@
 (function(window, document, undefined) {
 	'use strict';
-	angular.module('ular.ngResponsive.slider', [])
+	angular.module('ular.greyGoose.slider', [])
 	.provider('$slider', function () {
 		var defaults = this.defaults = {
 			indicators: false,
@@ -11,17 +11,17 @@
 			responsive: true,
 			breakpoints: { xs: 1, mobile: 2, tablet: 2, desktop: 3 },
 			slides: [],
-			autoPlay: true,
+			autoPlay: false,
 			pauseOnHover: true,
 		};
 		var screen = 'desktop';
-		var options, scope, $slider = {};
 		this.$get = ['$window', '$rootScope', '$timeout', function($window, $rootScope, $timeout){
 			function SliderFactory(config){
-				console.log('SliderFactory');
-				options = $slider.$options = angular.extend({}, defaults, config);
-				scope = $slider.$scope = options.scope && options.scope.$new() || $rootScope.$new();
+				var $slider = {};
+				var options = $slider.$options = angular.extend({}, defaults, config);
+				var scope = $slider.$scope = options.scope;
 				var container = findElement('slider-responsive');
+				var slideUl = findElement('.slides', container[0]);
 				var items;
 				var parentW = container[0].clientWidth;
 				scope.$slidesInViewport = 1;
@@ -31,17 +31,89 @@
 					scope.$slides = options.slides;
 					if(options.indicators) scope.$indicators = true;
 					if(options.controlNav) scope.$controlNav = true;
-					//scope.viewPort();
-					console.log(options);
-					container[0].style.transition = 'all '+(options.animationTransition / 1000)+'s ease-in-out';
+					scope.viewPort();
+					slideUl[0].style.transition = 'all '+(options.animationTransition / 1000)+'s ease-in-out';
 					container.on('mouseenter', scope.$onMouseEnter);
 					container.on('mouseleave', scope.$onMouseLeave);
 				};
+				angular.element($window).bind('resize', function(){
+					scope.viewPort();
+				});
+				scope.viewPort = function(){
+					var windowW = $window.innerWidth;
+					if(windowW >= 992){
+						screen = 'desktop';
+					}else if(768 <= windowW && windowW < 992){
+						screen = 'tablet';
+					}else if(480 <= windowW && windowW < 768){
+						screen = 'mobile';
+					}else{
+						screen = 'xs';
+					}
+					
+					if(angular.isDefined(options.breakpoints[screen])) scope.$slidesInViewport = options.breakpoints[screen];
+					scope.$indicator = (scope.$slidesInViewport === 1) ? scope.$slides.length : (scope.$slides.length - scope.$slidesInViewport + 1);
+					slideUl[0].style.width = ((100 / scope.$slidesInViewport) * scope.$slides.length)+'%';
+					scope.current = 0;
+					scope.$select(scope.current);
+				};
+				scope.$on('onRepeatLast', function(){
+					scope.$setItems();
+				});
+				scope.$setItems = function(){
+					items = container[0].querySelectorAll('.slide');
+					items[scope.current].classList.add('active');
+					for(var i = 0; i < scope.$slides.length; i++){
+						items[i].style.width = (100 / items.length)+'%';
+						if(options.slides[i].bgColor !== '' || options.slides[i].bgColor !== 'undefined'){
+							items[i].style.background = options.slides[i].bgColor;
+						}
+					}
+					if(options.autoPlay) scope.$play();
+				}
+				scope.$createIndicators = function(num){
+					return new Array(num);
+				};
+				scope.$isActive = function(state){
+					return scope.current === state;
+				}
+				scope.$select = function(index){
+					scope.current = index;
+					scope.moveSlide();
+				}
+				scope.$prev = function(){
+					scope.direction = -1;
+					scope.current = scope.current + scope.direction;
+					scope.moveSlide();
+				};
+				scope.$next = function(){
+					scope.direction = 1;
+					scope.current = scope.current + scope.direction;
+					scope.moveSlide();
+				};
+				scope.moveSlide = function(){
+					if(scope.direction === -1 && scope.current < 0){
+						scope.current = scope.$slides.length - scope.$slidesInViewport;
+					}
+					if(scope.direction === 1 && !items[scope.current + (scope.$slidesInViewport - 1)]){
+						scope.current = 0;
+					}
+					slideUl[0].style.marginLeft = '-'+((100 / scope.$slidesInViewport) * scope.current)+'%';
+				};
 				scope.$onMouseEnter = function(evt){
-					console.log('onMouseEnter');
+					if(options.pauseOnHover) scope.$stop();
 				};
 				scope.$onMouseLeave = function(evt){
-					console.log('onMouseLeave');
+					if(options.autoPlay) scope.$play();
+				};
+				scope.$play = function(){
+					scope.play = $timeout(function(){
+						scope.$next();
+						scope.$play();
+					}, options.interval);
+				};
+				scope.$stop = function(){
+					$timeout.cancel(scope.play);
 				};
 				$slider.init();
 				return $slider;
@@ -58,13 +130,18 @@
 			templateUrl : 'template/slider.html',
 			scope: true,
 			link: function(scope, element, attr, transclusion) {
-				console.log('directive');
 				var options = {
 					scope: scope
 				};
-				var slider = $slider();
-				slider.$options.scope = scope;
-				console.log(slider.$options);
+				if(angular.isDefined(scope.slider)){
+					angular.forEach(scope.slider, function(key, value) {
+						options[value] = key;
+					});
+				}
+				angular.forEach(scope.slider.slides, function(key, value) {
+					scope.slider.slides[value].content = $sce.trustAsHtml(key.content);
+				});
+				var sliderres = $slider(options);
 			}
 		};
 	}]).directive('onLastRepeat', function(){
@@ -74,5 +151,5 @@
 			}, 1);
 		};
 	});
-	angular.module('ular.ngResponsive', [ 'ular.ngResponsive.slider' ]);
+	angular.module('ular.greyGoose', [ 'ular.greyGoose.slider' ]);
 })(window, document);
